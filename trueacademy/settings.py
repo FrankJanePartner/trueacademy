ca8d75fe55f7c8dae8faec5a56e1a9c7fc0e1504
FrankJanePartner <partnermarvel55@gmail.com>
2026-08-13 00:00:45 +0100

    modified:   trueacademy/settings.py

--- trueacademy/settings.py
+++ trueacademy/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Load environment variables from .env if present
 load_dotenv(BASE_DIR / '.env')
 
-SECRET_KEY =  'django-insecure-af29)q1vxvghnqn)m8+@5-f3abnrb$1^q92yudog-5j=c3hk&z'
+SECRET_KEY='django-insecure-af29)q1vxvghnqn)m8+@5-f3abnrb$1^q92yudog-5j=c3hk&z'
 
 DEBUG = True
 ALLOWED_HOSTS = ['trueacademy-9bfv.vercel.app', 'trinityreu.academy', 'localhost:8000']
